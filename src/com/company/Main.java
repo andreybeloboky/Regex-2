e39f37c0364f148bdbc2e3c7cf8cb911720b6a89
Andrey Beloboky <andrei.beloboky@yandex.ru>
2022-04-27 23:34:12 +0300

    add regex

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,14 @@
 package com.company;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class Main {
-
     public static void main(String[] args) {
-        Pattern pattern1 = Pattern.compile("( [\".*\"] )");
-        String[] str1 = pattern1.split("This \"huge test\" is pointless");
+        String str = "The family was very wealthy in Elon's youth;Errol Musk once said,\"We had so much money at times we couldn't even close our safe\".Test test-test";
+        String[] str1 = str.split(" (?!.+\")|(?<!\".{2,}) |[;,.]");
         for (String value : str1) {
             System.out.println(value);
         }
-        Pattern pattern2 = Pattern.compile("[\s+]|-{2}");
-        String[] str2 = pattern2.split("Suzie Smith-Hopper test--hyphens");
-        for (String value : str2) {
-            System.out.print(value + ",");
-        }
-        Pattern pattern3 = Pattern.compile("[\s+]|-{2}");
-        String[] str3 = pattern3.split("I can't do it");
-        for (String value : str3) {
-            System.out.print(value + ",");
-        }
-        Pattern pattern4 = Pattern.compile("[\s;]+|-{2}");
-        String[] str4 = pattern4.split("Too long; didn't read");
-        for (String value : str4) {
-            System.out.print(value + ",");
-        }
+
     }
 }
